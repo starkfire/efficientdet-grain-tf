30f21301d387c7f1d35f9390d91886b79081a6e9
starkfire <plpantoja49@gmail.com>
2023-03-29 01:04:59 +0800

    fetch and unpack pre-trained weights

--- get_efficientdet_weights.sh
+++ get_efficientdet_weights.sh
@@ -1,2 +1,2 @@
 wget http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d0_coco17_tpu-32.tar.gz
-!tar -xvf efficientdet_d0_coco17_tpu-32.tar.gz -C ./pretrained
+tar -xvf efficientdet_d0_coco17_tpu-32.tar.gz -C ./pretrained
