70b9d3da0ed8ecf4380b9ec89eeed260dbc71cce
starkfire <plpantoja49@gmail.com>
2023-03-28 21:52:05 +0800

    feat: XML-to-CSV util

---
+++ utils/xml2csv.py
@@ -0,0 +1,45 @@
+"""
+
+Converting PASCAL VOC annotation files (.xml) to CSV
+
+"""
+
+import os
+import glob
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+def xml2csv(path):
+    xml_list = []
+
+    for xml_file in glob.glob(path + '/*.xml'):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+
+        for member in root.findall('object'):
+            value = (root.find('filename').text,
+                     int(root.find('size')[0].text),
+                     int(root.find('size')[1].text),
+                     member[0].text,
+                     int(member[4][0].text),
+                     int(member[4][1].text),
+                     int(member[4][2].text),
+                     int(member[4][3].text)
+                     )
+            xml_list.append(value)
+    
+    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    xml_df = pd.DataFrame(xml_list, columns=column_name)
+    
+    return xml_df
+
+
+def main():
+    for folder in ['train', 'test']:
+        image_path = os.path.join(os.getcwd(), ('images/' + folder))
+        xml_df = xml2csv(image_path)
+        xml_df.to_csv(('images/' + folder + '_labels.csv'), index=None)
+    
+    print('Success.')
+
+main()
